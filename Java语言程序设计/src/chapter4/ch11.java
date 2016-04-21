40d4c3f6d703b11a4f9106d88a29e750c644e5d6
Dcwjh <Dcwjh@DESKTOP-2F4UN1U>
2016-04-21 19:19:12 +0800

    方法1

--- Java语言程序设计/src/chapter4/ch11.java
+++ Java语言程序设计/src/chapter4/ch11.java
@@ -1,5 +1,52 @@
 package chapter4;
+import java.util.*;
 
 public class ch11 {
+	private static Scanner input;
 
+	public static void main(String[] args) {
+		input = new Scanner(System.in);
+		System.out.print("Enter a number(0-15):");
+		int number = input.nextInt();
+		System.out.println();
+		String a = " ";
+		if(number < 0 || number >= 15)
+			System.out.println(number + " is an invalue input");
+		if(number < 10 && number >= 0)
+		{
+			System.out.println("The hex value is:" + number);
+		}
+		else 
+		{
+			switch(number)
+			{
+		     	 case 10:
+			    	a = "A";
+			    	System.out.println("The hex value is " + a);
+				    break;
+		     	 case 11:
+		     		 a = "B";
+		     		 System.out.println("The hex value is " + a);
+		     		 break;
+		     	 case 12:
+		     		 a = "C";
+		     		 System.out.println("The hex value is " + a);
+		     		 break;
+		     	 case 13:
+		     		 a = "D";
+		     		 System.out.println("The hex value is " + a);
+		     		 break;
+		     	 case 14:
+		     		 a = "E";
+		     		 System.out.println("The hex value is " + a);
+		     		 break;
+		     	 case 15:
+		     		 a = "F";
+		     		 System.out.println("The hex value is " + a);
+		     		 break;
+			}
+				System.out.println("The hex value is " + a);
+		}
+	}
 }
+
